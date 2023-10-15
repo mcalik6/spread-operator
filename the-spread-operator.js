629a4492d926250c23150830ee8abfebdfe9ba24
mcalik6 <mcalik6@gmail.com>
2023-10-15 09:53:49 +0000

    spread operator

--- the-spread-operator.js
+++ the-spread-operator.js
@@ -4,9 +4,31 @@
  */
 
 // No spread operator
+let arr1 = [1, 2, 3];
+let arr2 = arr1;
+arr2.push(4);
+console.log("socand array", arr2);
+console.log("first array", arr1);
+
+
 
 // Copying an array
+let arr3 = [4, 5, 6];
+let arr4 = [...arr3];
 
+arr4.push(7);
+console.log("thirdarray", arr3);
+console.log("fourth array", arr4);
 // Copying an object
+let obj1 = { a: 1, b: 2, c: 3 };
+let obj2 = { ...obj1, d: 4};
+let obj3 = { ...obj1, b:5};
+console.log("obj1", obj1);
+console.log("obj2", obj2);
+console.log("obj3", obj3);
 
 // Copying only part of an array/object
+let arr5 = [ ...arr1, {...obj1 }, ...arr3, "x", "y", "z"];
+console.log(arr5);
+
+
